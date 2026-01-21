af17db5c21b8c499845f61e5f2815c15e1d35bb5
Deepak Singh <deepakksingh1202@gmail.com>
2026-01-21 17:42:48 +0530

    update css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import { HashRouter } from "react-router-dom";
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
