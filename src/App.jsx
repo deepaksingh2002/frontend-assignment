b56bc7def683f8e9b0843e840df01dcaf40be727
Deepak Singh <deepakksingh1202@gmail.com>
2026-01-21 14:21:07 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+import Footer from './components/Footer';
+
+const App = () => {
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <div className='container'>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
