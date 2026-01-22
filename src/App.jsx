59bbaa6b54457ea4d4f10e576b63ba61aa065191
Deepak Singh <157193532+deepaksingh2002@users.noreply.github.com>
2026-01-22 02:52:22 +0000

    App component structure and update main entry point for improved routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
-import Footer from './components/Footer';
+// Pages
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
-
   return (
-    <div>
+    <>
       <Header />
-      <Hero />
-      <div className='container'>
-        <Footer />
-      </div>
-    </div>
-  )
-}
 
-export default App
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
